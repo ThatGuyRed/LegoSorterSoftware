e64fdf34a42a2de17497142dad718d2f3c16de7c
Josh Chan <redgreenpro2@gmail.com>
2026-03-04 23:19:28 +1000

    new file:   Brickognize with Custom Dictionary.py 	new file:   categories.csv 	new file:   csvtest.py

---
+++ csvtest.py
@@ -0,0 +1,15 @@
+import csv
+
+
+reader = csv.DictReader(open('categories.csv', 'r', newline=''))
+categories = list(reader)
+
+
+def SearchCategory(category):
+    for item in categories:
+        if item['name'] == category:
+            return item['category']
+
+
+SearchCategory('Fence')
+categories.get('1')
