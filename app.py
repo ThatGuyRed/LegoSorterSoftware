7cf414924a2f70a90d113678091132da0bb38ff1
Josh Chan <redgreenpro2@gmail.com>
2026-03-11 19:42:26 +1000

    deleted:    Brickognize with Custom Dictionary.py 	modified:   app.py

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 # import Brickognize_with_Custom_Dictionary.py
 import sys
 from src import *
-from PyQt6.QtWidgets import *
 
-app = QApplication(sys.argv)
+import sys
+from PySide6.QtWidgets import *  # debug, change to only relevant modules in 1.0
+from PySide6.QtCore import QFile
+from main import Ui_LegoSorterEESS
+import os
+import subprocess
 
-# window = QWidget()
-# window.setWindowTitle("Lego Sorter EESS")
 
-label = QLabel("amogus")
-button = QPushButton("le button")
-button.show()
-# window.show()
-label.show()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_LegoSorterEESS()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.execTerm)
 
-app.exec()
+    def execTerm(self):
+        os.system("python Brickognize_with_Custom_Dictionary.py")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
